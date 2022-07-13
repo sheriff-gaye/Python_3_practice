c612963365bdeae4e77a6d58629507c204e52e3a
sheriff-gaye <sheriffgaye5@gmail.com>
2022-07-13 20:49:23 +0000

    py

---
+++ Problem_solving/cap.py
@@ -0,0 +1,5 @@
+def capitalize_word(word):
+    fir=word[0].upper()
+    return("{}{}".format(fir,word[1:]))
+
+print(capitalize_word("sheriff"))
---
+++ Problem_solving/countsheep.py
@@ -0,0 +1,14 @@
+def count_sheeps(sheep):
+    count=0
+
+    for i in sheep:
+        if i==True:
+            count+=1
+    return count
+
+print(count_sheeps([True,  True,  True,  False,
+          True,  True,  True,  True ,
+          True,  False, True,  False,
+          True,  False, False, True ,
+          True,  True,  True,  True ,
+          False, False, True,  True ]))
